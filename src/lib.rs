d2351aec3362667bdd3ebec371886de393776e03
skelegorg <62708878+skelegorg@users.noreply.github.com>
2022-03-01 16:11:03 -0500

    final commit for a while. taking a break

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,43 @@
 pub fn run(arguments: Vec<String>) {
-    let args = Arguments::new(&arguments);
+    let args_result = Arguments::new(&arguments);
+    if args_result.is_err() {
+        println!("xenon version {} | use xenon --help for more commands.", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+    let args: Arguments = args_result.unwrap();
+    match args.cmd[1].as_ref() {
+        "image" => image(args),
+        "restore" => restore(args),
+        "--help" => help(),
+        "--version" => println!("xenon version {}", env!("CARGO_PKG_VERSION")),
+        _ => println!("xenon: no such command \'{}\'", &args.cmd[1])
+    }
 }
+
+fn image(args: Arguments) {
+    // look for config folder location
+}
+
+fn restore(args: Arguments) {
+
+}
+
+fn help() {
+    println!("xenon version {}", env!("CARGO_PKG_VERSION"));
+    println!("Usage: xenon [command] [arguments] [flags]\n");
+    println!("Arguments:");
+    println!("  [command]   The command to execute.");
+    println!("  [arguments] The arguments passed to the command.");
+    println!("  [flags]     Options for the command.\n");
+    println!("Commands:");
+    println!("  image       Create an image of the current config directory.");
+    println!("  restore     Load an image.");
+}
+
 
 struct Arguments {
     cmd: Vec<String>,
-    verbose: bool,
+    flags: Vec<String>,
 }
 
 impl Arguments {
@@ -16,23 +49,16 @@ impl Arguments {
         let mut argss = Vec::new();
 
         for arg in args {
-            println!("{:?}", arg);
             match arg.chars().next() {
-                Some('-') => argss.push(arg.clone()),
+                Some('-') => {
+                    argss.push(arg.clone());
+                    cmd.push(arg.clone());
+                },
                 Some(_) => cmd.push(arg.clone()),
                 None => println!("error reading argument {}", arg)
             }
         }
-
-        let mut verb: bool = false;
-
-        for arg in argss {
-            match arg.as_ref() {
-                "-v" => verb = true,
-                "--verbose" => verb = true,
-                _ => {}
-            }
-        }
-        Ok(Arguments { cmd: cmd, verbose: verb })
+        
+        Ok(Arguments { cmd: cmd, flags: argss})
     }
 }
